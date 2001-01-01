98199be0f75ac4cd078b6205a57cfb78cb4bd3e8
Ahmadjon <Ahmadjon@MacBook-Pro-Ahrorbek-2.local>
2001-01-01 05:13:59 +0500

    Initial Commit

---
+++ Sof/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sof
+//
+//  Created by Ahmadjon on 01.01.01.
+//  Copyright © 2001 Dasturchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
